14b93bc0a31ac4b174bc2323373f665f6dcf80a4
Dayoon Kwon <dk1943@nyu.edu>
2023-07-11 03:38:28 -0700

    change fit name

--- R/kdm_calc.R
+++ R/kdm_calc.R
@@ -1,3 +1,29 @@
+#builds a fomula
+form = function(y,x){
+  return(as.formula(paste0(y,'~',x)))
+}
+
+#helper function to get effects from linear models on biomarkers
+get_effs = function(mod){
+  #input model object
+  #output is list of
+  #intercept, rmse, rsqaure
+
+  m = summary(mod)
+  res = data.frame(q=coef(mod)['(Intercept)'],k=coef(mod)[2],s=NA,r=NA)
+  if('glm' %in% attr(mod,'class')){
+    #sd of residuals is the RMSE
+    res$s = sd(mod$residuals)
+    #calculate s=pseudo r-squared
+    res$r = 1-(mod$deviance/mod$null.deviance)
+  }else{
+    res$s = m$sigma
+    res$r = m$r.squared
+  }
+
+  return(res)
+}
+
 #' Projecting KDM algorithm onto new data.
 #'
 #' @title kdm_calc
@@ -30,58 +56,61 @@
 kdm_calc = function (data, biomarkers, fit = NULL, s_ba2 = NULL) {
 
   dat = data
-  bm = biomarkers
-  bm_dat = t(select(dat, bm))
+  bm = biomarkers; rm(biomarkers)
 
-  rm(biomarkers)
+  design=svydesign(id=~1,weights=~1,data=dat)
 
   if (is.null(fit)) {
 
-    lm_age = t(apply(bm_dat, 1, function(x) {
-      sm = summary(lm(x ~ dat$age))
-      return(c(sm$sigma, sm$r.sq, sm$coef[1,1], sm$coef[2,]))
+    lm_age = lapply(bm,function(marker){
+      svyglm(form(marker,"age"),
+             design=design,
+             family=gaussian())
+    })
 
-    }))
 
-    colnames(lm_age) <- c("RMSE", "r_squared", "B_intercept", "B_age", "se(B)_age", "T_val_age", "pval_age")
-    lm_age <- as.data.frame(lm_age)
-
-    lm_age$r1 = abs((lm_age$B_age / lm_age$RMSE) * sqrt(lm_age$r_squared))
-    lm_age$r2 = abs((lm_age$B_age / lm_age$RMSE))
-    lm_age$n2 = (lm_age$B_age / lm_age$RMSE) * (lm_age$B_age / lm_age$RMSE)
+    agev = do.call(rbind,lapply(lm_age,get_effs)) %>%
+      mutate(bm = bm,
+             r1=abs((k/s)*sqrt(r)),
+             r2=abs(k/s),
+             n2=(k/s)^2); rm(lm_age)
 
     age_range = range(dat$age, na.rm = TRUE)
-    rchar = sum(lm_age$r1) / sum(lm_age$r2)
-    s_r = ((1 - (rchar * rchar)) / (rchar * rchar) * (((age_range[2] - age_range[1]) ^ 2) / (nrow(lm_age) * 12)))
+    rchar = sum(agev$r1)/sum(agev$r2)
+    s_r = ((1-(rchar^2))/(rchar^2))*(((age_range[2]-age_range[1])^2)/(12*nrow(agev)))
 
   }
 
   else {
 
-    lm_age = fit$lm_age
+    agev = fit$lm_age
     s_r = fit$s_r
 
   }
   #end dat conditional
 
-  n1 = bm_dat
-  for (r in 1:nrow(n1)) {
+  n1 = dat[,bm]
 
-    x = rownames(n1)[r]
-    n1[r,] = ((bm_dat[x,] - lm_age[x,"B_intercept"]) * (lm_age[x,"B_age"] / (lm_age[x,"RMSE"] * lm_age[x,"RMSE"])))
+  for(m in colnames(n1)){
+    row = which(agev$bm == m)
+    obs = (dat[,m] - agev[row,'q'])*(agev[row,'k']/(agev[row,'s']^2))
+    n1[,m]=(obs)
+    }
 
-  }
+  ba_nmiss = apply(n1,1,function(x) sum(is.na(x)))
+  ba_obs = length(bm) - ba_nmiss
+  BAe_n = rowSums(n1,na.rm=TRUE)
+  BAe_d = sum(agev$n2,na.rm=TRUE)
 
-  rm(r); rm(x)
+  dat = dat %>%
+    mutate(BA_eo = BAe_n/BAe_d,
+           BA_e = (BA_eo/(ba_obs))*length(bm))
 
-  BAe_n = apply(n1, 2, sum); rm(n1)
-  BAe_d = sum(lm_age$n2)
+  dat$BA_CA = unlist(dat$BA_e - dat$age)
+  t1 = (dat$BA_CA - mean(dat$BA_CA,na.rm=TRUE))^2
+  s2=mean(t1,na.rm=TRUE)
 
-  BAe = BAe_n / BAe_d
-  BA_CA = BAe - dat$age
-
-  s2 = sd(BA_CA, na.rm = TRUE) ^ 2
-  nobs = sum(!is.na(BA_CA))
+  nobs = sum(!is.na(dat$BA_CA))
 
   if (is.null(s_ba2)) {
 
@@ -95,10 +124,14 @@ kdm_calc = function (data, biomarkers, fit = NULL, s_ba2 = NULL) {
 
   }
 
-  dat$kdm = ((BAe_n) + (dat$age / (s_ba2))) / ((BAe_d) + (1 / (s_ba2)))
+  dat$kdm = unlist((BAe_n + (dat$age/c(s_ba2)))/(BAe_d+(1/c(s_ba2))))
+  dat$kdm = ifelse(ba_nmiss>2,NA,dat$kdm)
   dat$kdm_advance = dat$kdm - dat$age
+  dat$BA_eo = NULL
+  dat$BA_e = NULL
+  dat$BA_CA = NULL
 
-  fit = list(lm_age = lm_age, s_r = s_r, s_ba2 = s_ba2, s2 = s2, nobs = nobs)
+  fit = list(lm_age = agev, s_r = s_r, s_ba2 = s_ba2, s2 = s2, nobs = nobs)
 
   kdm = list(data = as.data.frame(dat), fit = fit)
   class(kdm) = append(class(kdm), "kdm")
