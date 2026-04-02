0564104305ba1f41a84859735a625e2f62db3388
TomNaber <tomnaber12@gmail.com>
2026-04-02 16:10:45 +0200

    Added variable dictating number of missing biomarkers permitted per subject before exclusion in KDM calculation to kdm_calc and kdm_nhanes, with optional verbose messages

--- R/kdm_nhanes.R
+++ R/kdm_nhanes.R
@@ -3,6 +3,8 @@
 #' @title kdm_nhanes
 #' @description Train KDM algorithm in NHANES III and project into NHANES IV.
 #' @param biomarkers A character vector indicating the names of the biomarkers included in the KDM Biological Age algorithm.
+#' @param n_allowed_missing_biomarkers Number of biomarkers per subject that are allowed missing before subject is excluded. Residual bioage variance is rescaled to number of missing values if greater than 0. Note that this can inflate noise and assumes missing biomarker residual variance to be high if other biomarkers have high residual variance.
+#' @param verbose A boolean to instruct whether to send messages about incomplete included and/or excluded data. Uses the substitute function to read name of dataframe.
 #' @return An object of class "kdm". This object is a list with two elements (data and fit). The dataset can be drawn by typing 'data'. The model can be drawn by typing 'fit'.
 #' @examples
 #' #KDM using NHANES
@@ -16,23 +18,47 @@
 #' @import dplyr
 
 
-kdm_nhanes = function (biomarkers) {
+kdm_nhanes = function(biomarkers, n_allowed_missing_biomarkers = 0, verbose = TRUE) {
 
   #calculate training KDM
-  train_fem = kdm_calc(data = NHANES3 %>%
-                         filter(age >= 30 & age <= 75 & pregnant == 0, gender == 2),
-                       biomarkers, fit = NULL, s_ba2 = NULL)
-  train_male = kdm_calc(data = NHANES3 %>%
-                          filter(age >= 30 & age <= 75 & pregnant == 0, gender == 1),
-                        biomarkers, fit = NULL, s_ba2 = NULL)
+  train_fem <- kdm_calc(
+    data = NHANES3 %>%
+      filter(age >= 30 & age <= 75 & pregnant == 0, gender == 2),
+    biomarkers = biomarkers,
+    fit = NULL,
+    s_ba2 = NULL,
+    n_allowed_missing_biomarkers = n_allowed_missing_biomarkers,
+    verbose = verbose
+  )
+  train_male <- kdm_calc(
+    data = NHANES3 %>%
+      filter(age >= 30 & age <= 75 & pregnant == 0, gender == 1),
+    biomarkers = biomarkers,
+    fit = NULL,
+    s_ba2 = NULL,
+    n_allowed_missing_biomarkers = n_allowed_missing_biomarkers,
+    verbose = verbose
+  )
 
   #calculate test modified KDM
-  test_fem = kdm_calc(data = NHANES4 %>%
-                        filter(gender == 2),
-                      biomarkers, fit = train_fem$fit, s_ba2 = train_fem$fit$s_ba2)
-  test_male = kdm_calc(data = NHANES4 %>%
-                         filter(gender == 1),
-                       biomarkers, fit = train_male$fit, s_ba2 = train_male$fit$s_ba2)
+  test_fem <- kdm_calc(
+    data = NHANES4 %>%
+      filter(gender == 2),
+    biomarkers = biomarkers,
+    fit = train_fem$fit,
+    s_ba2 = train_fem$fit$s_ba2,
+    n_allowed_missing_biomarkers = n_allowed_missing_biomarkers,
+    verbose = verbose
+  )
+  test_male <- kdm_calc(
+    data = NHANES4 %>%
+      filter(gender == 1),
+    biomarkers = biomarkers,
+    fit = train_male$fit,
+    s_ba2 = train_male$fit$s_ba2,
+    n_allowed_missing_biomarkers = n_allowed_missing_biomarkers,
+    verbose = verbose
+  )
 
   #combine calculated kdm
   test = rbind(test_fem$data, test_male$data)
